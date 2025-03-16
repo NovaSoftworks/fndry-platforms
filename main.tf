6e987909a538576e678184ae954ec47b26873c6a
Marc Lamy <29371248+marclamy@users.noreply.github.com>
2025-03-16 23:26:27 +0100

    initialize the platform module with a Google Cloud project with compute

--- main.tf
+++ main.tf
@@ -1,1 +1,21 @@
+data "terraform_remote_state" "foundation" {
+  backend = "gcs"
+  config = {
+    bucket = var.foundation_state_bucket
+    prefix = "tfstate/foundation"
+  }
+}
 
+output "foundation_outputs" {
+  value = data.terraform_remote_state.foundation.outputs
+}
+
+
+module "poc_prd" {
+  source = "./modules/platform"
+
+  parent_folder_id       = data.terraform_remote_state.foundation.outputs.prd_platforms_folder_id
+  billing_account_id     = var.billing_account
+  environment_short_name = "prd"
+  purpose                = "poc"
+}
